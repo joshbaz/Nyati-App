8c40b9f1ee288f6f5666bf178d5eb8947609ead0
Newton Mbugua <mymbugua@gmail.com>
2024-11-12 11:48:47 +0000

    removed react-native-orientation

--- src/screens/2MainScreens/WatchFilm.js
+++ src/screens/2MainScreens/WatchFilm.js
@@ -1,88 +1,80 @@
+import { ResizeMode, Video } from "expo-av"
+import React, { useEffect, useRef, useState } from "react"
 import {
-  StyleSheet,
-  Text,
-  View,
-  Animated,
-  TouchableOpacity,
-  TextInput,
-  Pressable,
-  KeyboardAvoidingView,
-  Platform,
   ActivityIndicator,
-  ScrollView,
-  Modal,
+  Animated,
   Dimensions,
   FlatList,
-} from "react-native";
-import React, { useEffect, useState, useRef } from "react";
-import { Stack, Box, HStack, VStack } from "@react-native-material/core";
-import { Ionicons } from "@expo/vector-icons";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { COLORS, FONTSFAMILIES } from "../../color/VariableColors";
-import { Formik } from "formik";
-import * as yup from "yup";
-import { BallIndicator } from "react-native-indicators";
-import CategoryHeader from "../../components/CategoryHeader";
-const { width, height } = Dimensions.get("window");
-import MoviesDB from "../../../assets/data/db.json";
-import UpcomingMovieCard from "../../components/UpcomingMovieCard";
-import ContinueWatchCard from "../../components/ContinueWatchCard";
-import FilmFundCard from "../../components/FilmFundCard";
-import FeaturedMovieCard from "../../components/FeaturedMovieCard";
-import { Entypo } from "@expo/vector-icons";
-import { Video, ResizeMode } from "expo-av";
-import Orientation from "react-native-orientation";
+  KeyboardAvoidingView,
+  Modal,
+  Platform,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native"
+import { BallIndicator } from "react-native-indicators"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { Box, HStack, Stack, VStack } from "@react-native-material/core"
+import { Ionicons } from "@expo/vector-icons"
+import { Entypo } from "@expo/vector-icons"
+import { Feather } from "@expo/vector-icons"
+import { Formik } from "formik"
+import * as yup from "yup"
+import MoviesDB from "../../../assets/data/db.json"
+import { COLORS, FONTSFAMILIES } from "../../color/VariableColors"
+import CategoryHeader from "../../components/CategoryHeader"
+import ContinueWatchCard from "../../components/ContinueWatchCard"
+import FeaturedMovieCard from "../../components/FeaturedMovieCard"
+import FilmFundCard from "../../components/FilmFundCard"
+import UpcomingMovieCard from "../../components/UpcomingMovieCard"
 
-import { Feather } from "@expo/vector-icons";
-
-
+const { width, height } = Dimensions.get("window")
 
 const WatchFilm = ({ navigation }) => {
-const [filmStatus, setFilmStatus] = useState({
-  currentTime: 0,
-  duration: 0.1,
-  paused: false,
-  overlay: false,
-  fullscreen: false,
-});
-    const [status, setStatus] = React.useState({});
-    
-    const video = React.useRef(null);
-    const urlVideo = React.useRef(null)
-    //load function
+  const [filmStatus, setFilmStatus] = useState({
+    currentTime: 0,
+    duration: 0.1,
+    paused: false,
+    overlay: false,
+    fullscreen: false,
+  })
+  const [status, setStatus] = React.useState({})
 
-    //timer
-      const getTime = (t) => {
-        const digit = (n) => (n < 10 ? `0${n}` : `${n}`);
-        // const t = Math.round(time);
-        const sec = digit(Math.floor(t % 60));
-        const min = digit(Math.floor((t / 60) % 60));
-        const hr = digit(Math.floor((t / 3600) % 60));
-        return hr + ":" + min + ":" + sec; // this will convert sec to timer string
-        // 33 -> 00:00:33
-        // this is done here
-        // ok now the theme is good to look
-    };
-    
-    const load = ({ duration }) => setFilmStatus(() => ({ ...filmStatus, duration: duration }));
-    const progress = ({ currentTime }) => {
-       console.log("running progress", currentTime);
-       setFilmStatus(() => ({ ...filmStatus, currentTime }));  
-   }
-        
-    
-     const  fullscreen = () => {
-        const { fullscreen } = filmStatus;
-        if (fullscreen) {
-          Orientation.lockToPortrait();
-        } else {
-          Orientation.lockToLandscape();
-        }
-         
-         setFilmStatus(() => ({ ...filmStatus, fullscreen: !fullscreen }));
-    };
-    
-    //console.log("status", status);
+  const video = React.useRef(null)
+  const urlVideo = React.useRef(null)
+  //load function
+
+  //timer
+  const getTime = (t) => {
+    const digit = (n) => (n < 10 ? `0${n}` : `${n}`)
+    // const t = Math.round(time);
+    const sec = digit(Math.floor(t % 60))
+    const min = digit(Math.floor((t / 60) % 60))
+    const hr = digit(Math.floor((t / 3600) % 60))
+    return hr + ":" + min + ":" + sec // this will convert sec to timer string
+    // 33 -> 00:00:33
+    // this is done here
+    // ok now the theme is good to look
+  }
+
+  const load = ({ duration }) =>
+    setFilmStatus(() => ({ ...filmStatus, duration: duration }))
+  const progress = ({ currentTime }) => {
+    console.log("running progress", currentTime)
+    setFilmStatus(() => ({ ...filmStatus, currentTime }))
+  }
+
+  const fullscreen = () => {
+    const { fullscreen } = filmStatus
+
+    setFilmStatus(() => ({ ...filmStatus, fullscreen: !fullscreen }))
+  }
+
+  //console.log("status", status);
   return (
     <SafeAreaView
       style={{
@@ -117,22 +109,22 @@ const [filmStatus, setFilmStatus] = useState({
             source={require("./reactapp.mp4")}
             onProgress={progress}
             useNativeControls
-            resizeMode={'contain'}
+            resizeMode={"contain"}
             onPlaybackStatusUpdate={(status) => setStatus(() => status)}
           />
         </View>
 
         <View style={styles.arrowBackBtn}>
           <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Feather name="arrow-left-circle" size={30} color="white" />
+            <Feather name='arrow-left-circle' size={30} color='white' />
           </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default WatchFilm;
+export default WatchFilm
 
 const styles = StyleSheet.create({
   videoTitleWrap: {
@@ -144,10 +136,10 @@ const styles = StyleSheet.create({
     color: "#ffffff",
     fontSize: 20,
   },
-    arrowBackBtn: {
-        color: '#ffffff',
-        position: 'absolute',
-        top: 20,
-        left:20,
+  arrowBackBtn: {
+    color: "#ffffff",
+    position: "absolute",
+    top: 20,
+    left: 20,
   },
-});
+})
